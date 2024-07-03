6ae1a9c9868287be0f76b52d90587989cb0b737c
Aaron Shaw <shawaj@gmail.com>
2024-07-03 17:07:26 +0100

    Update wingbits_installer.sh

--- wingbits_installer.sh
+++ wingbits_installer.sh
@@ -12,6 +12,12 @@ curl -1sLf 'https://repositories.timber.io/public/vector/cfg/setup/bash.deb.sh' 
 bash -c "$(curl -L https://setup.vector.dev)"
 apt-get -y install vector
 
+# install tar1090
+if [[ -z "$NO_TAR1090" ]] ; then
+    wget -O tar1090-install.sh https://raw.githubusercontent.com/wiedehopf/tar1090/master/install.sh
+    bash tar1090-install.sh /run/readsb
+fi
+
 # install graphs1090
 repo="https://github.com/wiedehopf/graphs1090"
 ipath=/usr/share/graphs1090
