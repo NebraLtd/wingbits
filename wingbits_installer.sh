51e381b06d4b69f417a2cabc9b29daefd3c141c4
Aaron Shaw <shawaj@gmail.com>
2024-07-03 13:49:32 +0100

    Update wingbits_installer.sh

--- wingbits_installer.sh
+++ wingbits_installer.sh
@@ -33,7 +33,7 @@ mkdir -p /usr/share/graphs1090/installed
 mkdir -p /var/lib/graphs1090/scatter
 
 apt install --no-install-recommends --no-install-suggests -y \
-    git rrdtool wget unzip bash-builtins collectd-core libpython3.10
+    git rrdtool wget unzip bash-builtins collectd-core libpython3.9
 wget --timeout=30 -q -O /tmp/master.zip https://github.com/wiedehopf/graphs1090/archive/master.zip && unzip -q -o master.zip
 cd /tmp/graphs1090-master
 
