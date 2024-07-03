d17b0ff621bb43f36c4b9ab689937bf44a82000e
Aaron Shaw <shawaj@gmail.com>
2024-07-03 12:54:16 +0100

    Create wingbits_installer.sh

---
+++ wingbits_installer.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+arch="$(dpkg --print-architecture)"
+echo System Architecture: $arch
+
+cd /tmp
+
+curl -1sLf 'https://repositories.timber.io/public/vector/cfg/setup/bash.deb.sh' | sudo -E bash
+bash -c "$(curl -L https://setup.vector.dev)"
+apt-get -y install vector
