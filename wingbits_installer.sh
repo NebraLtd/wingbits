d263f016519a449df312bcf86ee366ac57967965
Aaron Shaw <shawaj@gmail.com>
2024-07-04 03:05:06 +0100

    add tar1090

--- wingbits_installer.sh
+++ wingbits_installer.sh
@@ -11,3 +11,9 @@ cd /tmp
 curl -1sLf 'https://repositories.timber.io/public/vector/cfg/setup/bash.deb.sh' | sudo -E bash
 bash -c "$(curl -L https://setup.vector.dev)"
 apt-get -y install vector
+
+# install tar1090
+if [[ -z "$NO_TAR1090" ]] ; then
+    wget -O tar1090-install.sh https://raw.githubusercontent.com/wiedehopf/tar1090/master/install.sh
+    bash tar1090-install.sh /run/readsb
+fi
